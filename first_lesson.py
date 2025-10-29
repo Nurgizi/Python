746421d60c63deb1ea56f1a093d3ca51cdcd0a80
Nurgiza_Mirtemirova <nurgiza.mi270501@gmail.com>
2025-10-29 18:21:25 +0200

    любой комментарий

---
+++ first_lesson.py
@@ -0,0 +1,21 @@
+# print("Hello Nurgiza")
+# question1 = input("what is your name ?")
+# print("My name is", question1)
+# question2 = input('Nice to meet you, What are you looking for?')
+# print("I need ", question2)
+# question3 = input("We can find different candidates from Asia and Turkiye, can you help me from where do you want to have candidates?")
+# print("I want candidates from", question3)
+# question4 = input ("It;s good choice! For which period do you want? ")
+# print("For", question4)
+# question5 = input("It's a great choice. We can suggest you new program 3+3") 
+# print("What is it? ")
+# question6 = input("Your candidates can come for a seasonal job within 2 months and prolong for extra 3 months.Are you interested?")
+# print(question6)
+
+
+# Lesson 2 Переменные
+
+int (100)
+# это целые числа
+
+float(2.5)
