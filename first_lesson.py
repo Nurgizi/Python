807762c1b3bff6f2dd45cff54586af5685f43a4d
Nurgiza_Mirtemirova <nurgiza.mi270501@gmail.com>
2025-10-29 22:22:10 +0200

    two new files

--- first_lesson.py
+++ first_lesson.py
@@ -10,8 +10,10 @@
 # question5 = input("It's a great choice. We can suggest you new program 3+3") 
 # print("What is it? ")
 # question6 = input("Your candidates can come for a seasonal job within 2 months and prolong for extra 3 months.Are you interested?")
-# print(question6)
-
+# # print(question6)
+# question7 = input("How many candidates do you need? Our comission is 1 per person")
+# print ('I need', question7)
+# print (1*5)
 
 # Lesson 2 Переменные
 
@@ -19,3 +21,45 @@ int (100)
 # это целые числа
 
 float(2.5)
+# это дробные
+
+# Переменные пишутся с маленькой буквы и не начинается с цифры. Не начинается с пробела, если несколько слов, то нужно писать с _ или с заглавной буквы слитно
+# name = 
+# age = 
+# price1 =
+# customer_age =
+# customerName = 
+# 13name = not valid
+# name = "John"
+# question = name
+# print(question)
+
+# num1 = 1
+# num1 = num1 + 1
+# num1 = num1 * 3
+# print(num1)
+# конечный итог будет 6, так как Пайтон считает сверху вниз и делает последовательные матем. исчисления.
+
+# num5 = 44.454
+# num1 = 1
+# print(num1)
+# type_of_num5 = type(num5)
+# print(type_of_num5)
+# Как здесь надо 
+
+
+# print(type(num5))
+
+
+# num1 = 5
+# num2 = 22
+# num3 = "3.4"
+# # мы можем делать вычитание, умножения с цифрамию В ковычках текст цисло не можем минусовать, толко *
+# dif = num2 - num1
+# print(dif)
+
+# dif2 = num3 * num1
+# print(dif2)
+
+ageIsAllowed = True
+print(ageIsAllowed)
